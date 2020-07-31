e68d51a3a789c40b4a4600eecddd60a358c99f12
Daniel Ji <Jidaniel1234@gmail.com>
2020-07-31 00:43:11 -0700

    new ui features/inputs and backend improvements

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,27 +1,37 @@
 const express = require('express');
 const fs = require('fs');
 const { spawn } = require('child_process');
-const multer = require('multer');
-const upload = multer({ dest: 'src/codes/uploads'});
 const router = express.Router();
+
+const multer = require('multer');
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        cb(null, 'dbandbash/codes/uploads')
+    },
+    filename: (req, file, cb) => {
+        cb(null, file.originalname)
+    }
+})
+const upload = multer({ storage: storage });
 
 // may have to refactor
 // needs validation
 
 router.post('/blastp/file', upload.single('fasta'), (req, res, next) => {
     console.log(req.file);
-    const response = spawn('./src/codes/NCBI_blast', ['-LisAln', `uploads/${req.file.filename}`]);
+    const response = spawn('./dbandbash/codes/NCBI_blast', ['-LisAln', `uploads/${req.file.filename}`]);
     response.stdout.on("data", data => {
         process.stdout.write(data.toString());
     })
     response.stderr.on("data", data => {
         process.stderr.write(data.toString());
     })
+    response.on("exit", () => res.sendStatus(200));
 });
 
 router.post('/blastp/name', (req, res) => {
     let proteinName = String(req.body.proteinName);
-    if (proteinName.length !== 0) {
+    if (proteinName.length !== 0 && proteinName.length < 100) {
         const response = spawn('./src/codes/NCBI_blast', ['-LisAln', proteinName]);
         response.stdout.on("data", data => {
             process.stdout.write(data.toString());
@@ -30,6 +40,23 @@ router.post('/blastp/name', (req, res) => {
             process.stderr.write(data.toString());
         })
     }
+})
+
+router.post('/blastp/text', (req, res) => {
+    let fastaText = String(req.body.fastaText);
+    if (fastaText.length !== 0 && fastaText.length < 10000) {
+        fs.writeFile('./dbandbash/codes/uploads/test.fasta', fastaText, err => {
+            console.log(err);
+        })
+    }
+    const response = spawn('./dbandbash/codes/NCBI_blast', ['-LisAln', `uploads/test.fasta`]);
+    response.stdout.on("data", data => {
+        process.stdout.write(data.toString());
+    })
+    response.stderr.on("data", data => {
+        process.stderr.write(data.toString());
+    })
+    response.on("exit", () => res.sendStatus(200));
 })
 
 module.exports = router;
--- frontend/src/components/BlastRequest.js
+++ frontend/src/components/BlastRequest.js
@@ -2,7 +2,7 @@ import React, { Component } from 'react'
 import axios from 'axios'
 import Header from './Header';
 
-import { Button, Radio, RadioGroup, FormControlLabel, TextField} from '@material-ui/core'; 
+import { Button, TextField} from '@material-ui/core'; 
 
 class BlastRequest extends Component {
     
@@ -11,39 +11,61 @@ class BlastRequest extends Component {
     
         this.state = {
             result: '',
-            fileOrName: 'name',
             fastaInput: '',
             fastaInputTitle: '',
             proteinName: '',
+            fastaText: '',
+            nameFilePaste: '',
+            rangeStart: '',
+            rangeEnd: ''
         }
 
         this.runscript = this.runscript.bind(this);
-        this.updateFileOrName = this.updateFileOrName.bind(this);
         this.updateFile = this.updateFile.bind(this);
         this.updateName = this.updateName.bind(this);
+        this.updateFastaPaste = this.updateFastaPaste.bind(this);
+        this.clearFile = this.clearFile.bind(this);
+        this.updateRangeStart = this.updateRangeStart.bind(this);
+        this.updateRangeEnd = this.updateRangeEnd.bind(this);
     }
 
     //need to add validation
     runscript() {
-        if (this.state.fileOrName === 'name') {
-            if (this.state.proteinName !== '') {
-                axios.post('http://localhost:3000/api/blastp/name', {
-                    proteinName: this.state.proteinName
-                })
-                .then(response => {
-                    this.setState({result: response.data})
-                })
-                .catch(error => {
-                    this.setState({result: error.data})
-                })
-            }
-        } else {
+        if (this.state.proteinName !== '') {
+            axios.post('http://localhost:3000/api/blastp/name', {
+                proteinName: this.state.proteinName,
+                rangeStart: this.state.rangeStart,
+                rangeEnd: this.state.rangeEnd
+            })
+            .then(response => {
+                this.setState({result: response.data})
+            })
+            .catch(error => {
+                this.setState({result: error.data})
+            })
+        } else if (this.state.fastaText === '') {
             let blastForm = new FormData();
             blastForm.append('fasta', this.state.fastaInput);
             axios.post('http://localhost:3000/api/blastp/file', blastForm, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
+                },
+                data: {
+                    rangeStart: this.state.rangeStart,
+                    rangeEnd: this.state.rangeEnd
                 }
+            })
+            .then(response => {
+                this.setState({result: response.data})
+            })
+            .catch(error => {
+                this.setState({result: error.data})
+            })
+        } else {
+            axios.post('http://localhost:3000/api/blastp/text', {
+                fastaText: this.state.fastaText, 
+                rangeStart: this.state.rangeStart,
+                rangeEnd: this.state.rangeEnd
             })
             .then(response => {
                 this.setState({result: response.data})
@@ -54,44 +76,68 @@ class BlastRequest extends Component {
         }
     }
 
-    updateFileOrName(event) {
-        this.setState({fileOrName: event.target.value});
-    }
-
     updateFile(event) {
         if (typeof event.target.files[0] !== 'undefined') {
             this.setState({fastaInput: event.target.files[0], fastaInputTitle: event.target.files[0].name})
         }
     }
 
+    clearFile() {
+        this.setState({fastaInput: '', fastaInputTitle: ''})
+    }
+
     updateName(event) {
         this.setState({proteinName: event.target.value})
+    }
+
+    updateFastaPaste(event) {
+        this.setState({fastaText: event.target.value})
+    }
+
+    updateRangeStart(event) {
+        this.setState({rangeStart: event.target.value})
+    }
+
+    updateRangeEnd(event) {
+        this.setState({rangeEnd: event.target.value})
     }
 
     render() {
         return (
             <div className="BlastRequest">
-                <Header title="LisAln Blast" />
-                <RadioGroup aria-label="FileOrName" name="FileOrName" value={this.state.fileOrName} onChange={this.updateFileOrName}>
-                    <FormControlLabel value="file" control={<Radio color="primary"/>} label="Upload File" />
-                    <FormControlLabel value="name" control={<Radio color="primary"/>} label="Enter Protein Name" />
-                </RadioGroup>
-                {this.state.fileOrName === 'name' ?
-                <TextField label="Protein Name" variant="outlined" value={this.state.proteinName} onChange={this.updateName}/> :
-                <div style={{display: 'flex', justifyContent: 'center', flexDirection: 'column'}}> 
-                <Button variant="contained" component="label">Upload fasta 
-                    <input
-                        accept=".txt,.fasta"
-                        type="file"
-                        name='fasta'
-                        style={{ display: "none" }}
-                        onChange={this.updateFile}
+                <Header size="2rem" title="LisAln Blast Request" />
+                <div className="inputField">
+                    <Header size="1rem" title="Enter one of the three:" />
+                    <div className="rowInput">
+                        <TextField label="Protein Name Here" variant="outlined" value={this.state.proteinName} onChange={this.updateName}/>
+                        <Button disableElevation variant="contained" component="label">Upload Fasta {this.state.fastaInputTitle !== '' && `(Selected file: ${this.state.fastaInputTitle})`}
+                            <input
+                                accept=".txt,.fasta"
+                                type="file"
+                                name='fasta'
+                                style={{ display: "none" }}
+                                onChange={this.updateFile}
+                            />
+                        </Button>
+                        {this.state.fastaInputTitle !== '' && <Button disableElevation variant="contained" onClick={this.clearFile}>Clear</Button>}
+                    </div>
+                    <TextField
+                            label="Paste Fasta Here"
+                            multiline
+                            rows = "8"
+                            variant="outlined"
+                            value={this.state.fastaText}
+                            onChange={this.updateFastaPaste}
                     />
-                </Button>
-                {this.state.fastaInputTitle !== '' && <p>Current uploaded file: {this.state.fastaInputTitle}</p>}
                 </div>
-                }
-                <Button variant="contained" disableElevation onClick={this.runscript}>Go</Button>
+                <div className="inputField">
+                    <Header size="1rem" title="Range" />
+                    <div className="rowInput">
+                        <TextField label="Starting #" variant="outlined" value={this.state.rangeStart} onChange={this.updateRangeStart}/>
+                        <TextField label="Ending #" variant="outlined" value={this.state.rangeEnd} onChange={this.updateRangeEnd}/>
+                    </div>
+                </div>
+                <Button className="sendButton" variant="contained" disableElevation onClick={this.runscript}>Go</Button>
                 {this.state.result}
             </div>
         )
