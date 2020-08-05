1566ae3149b1b47d380e3735ce120b0c1d920405
Daniel Ji <Jidaniel1234@gmail.com>
2020-08-05 01:18:18 -0700

    fixed upload and image bugs, multi option handling, worked on scroll and outputting mutliple files

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -2,7 +2,7 @@ import React from 'react'
 
 function Header(props) {
     return (
-        <div className="Header" style={{fontSize: props.size}}>
+        <div className="Header" style={{fontSize: props.size, marginBottom: (props.lessMargin && '3vh') || (props.moreMargin && '7vh')}}>
             <h1>{props.title}</h1>
         </div>
     )
