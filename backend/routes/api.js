4f990897debb1d4c929b57c490c2a4702a2439c7
Daniel Ji <Jidaniel1234@gmail.com>
2020-08-05 01:18:18 -0700

    fixed upload and image bugs, multi option handling, worked on scroll and outputting mutliple files

--- backend/routes/api.js
+++ backend/routes/api.js
@@ -1,8 +1,8 @@
 const express = require('express');
 const fs = require('fs');
+const path = require('path');
 const { spawn } = require('child_process');
 const router = express.Router();
-
 const multer = require('multer');
 const storage = multer.diskStorage({
     destination: (req, file, cb) => {
@@ -13,32 +13,19 @@ const storage = multer.diskStorage({
     }
 })
 const upload = multer({ storage: storage });
+const filesuffixes = ['_landmark_homolo_aln.txt.gif', '_landmark_homolo_aln_phylotree_dendo.xls.gif', '_blastp_landmark_alnh.txt.gif', '_blastp_landmark_alnh_phylotree_dendo.xls.gif'];
 
 // may have to refactor
 // needs validation
 
 router.post('/blastp/file', upload.single('fasta'), (req, res, next) => {
-    console.log(req.file);
-    const response = spawn('./dbandbash/codes/NCBI_blast', ['-LisAln', `uploads/${req.file.filename}`]);
-    response.stdout.on("data", data => {
-        process.stdout.write(data.toString());
-    })
-    response.stderr.on("data", data => {
-        process.stderr.write(data.toString());
-    })
-    response.on("exit", () => res.sendStatus(200));
+    runAndOutput(`uploads/${req.file.filename}`, req, res);
 });
 
 router.post('/blastp/name', (req, res) => {
     let proteinName = String(req.body.proteinName);
     if (proteinName.length !== 0 && proteinName.length < 100) {
-        const response = spawn('./src/codes/NCBI_blast', ['-LisAln', proteinName]);
-        response.stdout.on("data", data => {
-            process.stdout.write(data.toString());
-        })
-        response.stderr.on("data", data => {
-            process.stderr.write(data.toString());
-        })
+        runAndOutput(proteinName, req, res);
     }
 })
 
@@ -49,14 +36,43 @@ router.post('/blastp/text', (req, res) => {
             console.log(err);
         })
     }
-    const response = spawn('./dbandbash/codes/NCBI_blast', ['-LisAln', `uploads/test.fasta`]);
+    runAndOutput('uploads/test.fasta', req, res);
+})
+
+//returning information to client and console
+const runAndOutput = (input, req, res) => {
+    let response;
+    if (Number.isInteger(parseInt(req.body.rangeStart)) && Number.isInteger(parseInt(req.body.rangeEnd))) {
+        response = spawn('./dbandbash/codes/NCBI_blast', ['-LisAln', '-range', (parseInt(req.body.rangeStart)), (parseInt(req.body.rangeEnd)), input]);   
+    } else {
+        console.log('asdfasdf');
+        console.log(input);
+        response = spawn('./dbandbash/codes/NCBI_blast', ['-LisAln', input]);
+    }
     response.stdout.on("data", data => {
         process.stdout.write(data.toString());
     })
     response.stderr.on("data", data => {
         process.stderr.write(data.toString());
     })
-    response.on("exit", () => res.sendStatus(200));
-})
+    response.on("exit", () => {
+        let filenameprefix;
+        let url = [];
+        if (req.file !== undefined) {
+            filenameprefix = req.file.filename.replace(/\.[^/.]+$/, "").toLowerCase();
+        } else if (req.body.proteinName !== undefined){
+            filenameprefix = req.body.proteinName.toLowerCase();
+        } else {
+            filenameprefix = 'test1';
+        }
+        filesuffixes.forEach((suffix) => {
+            fs.copyFile(`./dbandbash/codes/${filenameprefix}${suffix}`, `./public/images/${filenameprefix}${suffix}`, err => {
+                console.log(err);
+            })
+            url.push(`/public/images/${filenameprefix}${suffix}`);
+        });
+        res.send({url: url})
+    });
+}
 
 module.exports = router;
--- frontend/src/components/BlastRequest.js
+++ frontend/src/components/BlastRequest.js
@@ -4,6 +4,8 @@ import Header from './Header';
 
 import { Button, TextField} from '@material-ui/core'; 
 
+import smoothscroll from 'smoothscroll-polyfill';
+
 class BlastRequest extends Component {
     
     constructor(props) {
@@ -13,67 +15,82 @@ class BlastRequest extends Component {
             result: '',
             fastaInput: '',
             fastaInputTitle: '',
+            fastaInputKey: Math.random().toString(36),
             proteinName: '',
             fastaText: '',
             nameFilePaste: '',
             rangeStart: '',
-            rangeEnd: ''
+            rangeEnd: '',
+            serverUrl: 'http://localhost:3000',
+            showScrollIndicator: true
         }
 
         this.runscript = this.runscript.bind(this);
+        this.runscriptRespHandle = this.runscriptRespHandle.bind(this);
         this.updateFile = this.updateFile.bind(this);
+        this.clearFile = this.clearFile.bind(this);
         this.updateName = this.updateName.bind(this);
         this.updateFastaPaste = this.updateFastaPaste.bind(this);
-        this.clearFile = this.clearFile.bind(this);
         this.updateRangeStart = this.updateRangeStart.bind(this);
         this.updateRangeEnd = this.updateRangeEnd.bind(this);
+        this.scrollToGo = this.scrollToGo.bind(this);
+    }
+
+    componentDidMount() {
+        smoothscroll.polyfill();
     }
 
     //need to add validation
     runscript() {
         if (this.state.proteinName !== '') {
-            axios.post('http://localhost:3000/api/blastp/name', {
+            axios.post(this.state.serverUrl + '/api/blastp/name', {
                 proteinName: this.state.proteinName,
                 rangeStart: this.state.rangeStart,
                 rangeEnd: this.state.rangeEnd
             })
             .then(response => {
-                this.setState({result: response.data})
+                this.runscriptRespHandle(response);
             })
             .catch(error => {
                 this.setState({result: error.data})
             })
-        } else if (this.state.fastaText === '') {
+        } else if (this.state.fastaInput !== '') {
             let blastForm = new FormData();
             blastForm.append('fasta', this.state.fastaInput);
-            axios.post('http://localhost:3000/api/blastp/file', blastForm, {
+            blastForm.append('rangeStart', this.state.rangeStart);
+            blastForm.append('rangeEnd', this.state.rangeEnd);
+            axios.post(this.state.serverUrl + '/api/blastp/file', blastForm, {
                 headers: {
                     'Content-Type': 'multipart/form-data'
-                },
-                data: {
-                    rangeStart: this.state.rangeStart,
-                    rangeEnd: this.state.rangeEnd
                 }
             })
             .then(response => {
-                this.setState({result: response.data})
+                this.runscriptRespHandle(response);
             })
             .catch(error => {
                 this.setState({result: error.data})
             })
-        } else {
-            axios.post('http://localhost:3000/api/blastp/text', {
+        } else if (this.state.fastaText !== '') {
+            axios.post(this.state.serverUrl + '/api/blastp/text', {
                 fastaText: this.state.fastaText, 
                 rangeStart: this.state.rangeStart,
                 rangeEnd: this.state.rangeEnd
             })
             .then(response => {
-                this.setState({result: response.data})
+                this.runscriptRespHandle(response);
             })
             .catch(error => {
                 this.setState({result: error.data})
             })
         }
+    }
+
+    runscriptRespHandle(response) {
+        this.setState({result: response.data.url.map((image, index) => {
+            return <img key={`${this.state.serverUrl + image}?${Date.now()}`} alt="alignment result" src={`${this.state.serverUrl + image}?${Date.now()}`}/>
+        })}, () => {
+            setTimeout(() => document.querySelector('.imageResults').scrollIntoView({ behavior: 'smooth' }), 500)
+        })
     }
 
     updateFile(event) {
@@ -82,8 +99,8 @@ class BlastRequest extends Component {
         }
     }
 
-    clearFile() {
-        this.setState({fastaInput: '', fastaInputTitle: ''})
+    clearFile(event) {
+        this.setState({fastaInputKey: Math.random().toString(36), fastaInput: '', fastaInputTitle: ''})
     }
 
     updateName(event) {
@@ -102,43 +119,54 @@ class BlastRequest extends Component {
         this.setState({rangeEnd: event.target.value})
     }
 
+    scrollToGo() {
+        this.setState({showScrollIndicator: false}, () => {
+            document.querySelector('.sendButton').scrollIntoView({ behavior: 'smooth' })
+        })
+    }
+
     render() {
         return (
             <div className="BlastRequest">
                 <Header size="2rem" title="LisAln Blast Request" />
                 <div className="inputField">
-                    <Header size="1rem" title="Enter one of the three:" />
+                    <Header lessMargin size="1rem" title="Enter one of the three:"/>
+                    <Header moreMargin size="0.4rem" title="(If multiple selections filled, will default to Protein Name, then File Upload, and then Pasted Fasta)" />
                     <div className="rowInput">
                         <TextField label="Protein Name Here" variant="outlined" value={this.state.proteinName} onChange={this.updateName}/>
                         <Button disableElevation variant="contained" component="label">Upload Fasta {this.state.fastaInputTitle !== '' && `(Selected file: ${this.state.fastaInputTitle})`}
                             <input
+                                key={this.state.fastaInputKey}
                                 accept=".txt,.fasta"
                                 type="file"
                                 name='fasta'
                                 style={{ display: "none" }}
-                                onChange={this.updateFile}
+                                onChange={(event) => this.updateFile(event)}
+                                ref={ref => (this.fileInput = ref)}
                             />
                         </Button>
                         {this.state.fastaInputTitle !== '' && <Button disableElevation variant="contained" onClick={this.clearFile}>Clear</Button>}
                     </div>
                     <TextField
-                            label="Paste Fasta Here"
-                            multiline
-                            rows = "8"
-                            variant="outlined"
-                            value={this.state.fastaText}
-                            onChange={this.updateFastaPaste}
+                        label="Paste Fasta Here"
+                        multiline
+                        rows = "8"
+                        variant="outlined"
+                        value={this.state.fastaText}
+                        onChange={this.updateFastaPaste}
                     />
                 </div>
                 <div className="inputField">
-                    <Header size="1rem" title="Range" />
+                    <Header size="1rem" title="Range (optional)" />
                     <div className="rowInput">
                         <TextField label="Starting #" variant="outlined" value={this.state.rangeStart} onChange={this.updateRangeStart}/>
                         <TextField label="Ending #" variant="outlined" value={this.state.rangeEnd} onChange={this.updateRangeEnd}/>
                     </div>
                 </div>
                 <Button className="sendButton" variant="contained" disableElevation onClick={this.runscript}>Go</Button>
-                {this.state.result}
+                <br/>
+                <div className="imageResults">{this.state.result}</div>
+                <div className={`scrollIndicator ${this.state.showScrollIndicator ? 'shown': 'fadeOut'}`} onClick={this.scrollToGo}><span/></div>
             </div>
         )
     }
