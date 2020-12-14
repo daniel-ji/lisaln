524b8c7e2120628c3f9c0751ebb2f0a85c92a508
Daniel Ji <Jidaniel1234@gmail.com>
2020-12-14 15:56:19 -0800

    contact, about, and design updates

--- frontend/src/components/Header.js
+++ frontend/src/components/Header.js
@@ -6,7 +6,7 @@ import '../styles/hint.min.css';
 function Header(props) {
     return (
         <div className="Header" style={{fontSize: props.size, margin: props.margin, maxWidth: props.maxWidth}}>
-            <h1 className={props.className}>{props.title}</h1>
+            <h1 onClick={props.onClick} className={props.className}>{props.title}</h1>
             {props.help !== undefined && <div className={`${(props.leftHelp ? "hint--left" : "hint--right")} hint--rounded hint--bounce hintContainer`} aria-label={props.help}><img src={helpLogo} alt="Info icon"/></div>}
         </div>
     )
